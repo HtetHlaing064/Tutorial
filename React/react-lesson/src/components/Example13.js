437f79ea6a9ef2e8ffa44e59b4eb739f2cccb543
HtetHlaing064 <htethtethlaing064@gmail.com>
2025-06-04 14:56:04 +0630

    react-style

---
+++ React/react-lesson/src/components/Example13.js
@@ -0,0 +1,11 @@
+"use client";
+import styles from "./Example13.module.css";
+export default function UserProfile() {
+  return (
+    <div>
+      <h1 className={styles.h1}>User Profile</h1>
+      <p className={styles.name}>Name:Mg Mg</p>
+      <input className={styles.userInput} />
+    </div>
+  );
+}
