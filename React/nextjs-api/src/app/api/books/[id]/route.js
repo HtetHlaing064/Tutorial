bfcb060b5c56a6e2833d1bd8ca0dfbff61e7c365
HtetHlaing064 <htethtethlaing064@gmail.com>
2025-06-23 10:35:27 +0630

    api update

--- React/nextjs-api/src/app/api/books/[id]/route.js
+++ React/nextjs-api/src/app/api/books/[id]/route.js
@@ -1,35 +1,61 @@
 import { NextResponse } from "next/server";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  title: yup.string().required("Title is required"),
+  author: yup.string().required("fatherName is required"),
+  year: yup.string().required("Year is required"),
+});
 
 export async function PUT(req, { params }) {
-  const bookId = params.id;
-  const body = await req.json();
-  return NextResponse.json({
-    meassage: "Book is successfully updated",
-    bookId,
-    bodyData: body,
-  });
+  try {
+    const bookId = params.id;
+    const body = await req.json();
+    await schema.validate(body, { abortEarly: false });
+    return NextResponse.json({
+      message: "Student is successfully update",
+      bookId,
+      bodyData: body,
+    });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      return NextResponse.json({
+        message: "Validation Failed",
+        errors: error.inner.map((e) => ({
+          path: e.path,
+          message: e.message,
+        })),
+      });
+    }
+
+    return NextResponse.json(
+      {
+        message: "Unexpected Error",
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
 }
 
 export async function DELETE(req, { params }) {
   const bookId = params.id; //Get URL params field
-  const body = await req.json();
+  
   return NextResponse.json({
     meassage: "Book is successfully deleted",
     bookId,
-    bodyData: body,
+    
   });
 }
 
-export async function GET(req,{params}) {
+export async function GET(req, { params }) {
   const bookId = params.id;
   const book = {
-    id:bookId,
-    title:"Wuthering Heights",
-    author:"Emily Brontë ",
-    gender:"Male",
-    year:1847
+    id: bookId,
+    title: "Wuthering Heights",
+    author: "Emily Brontë ",
+    gender: "Male",
+    year: 1847,
   };
-  return NextResponse.json(
-    book
-  );
+  return NextResponse.json(book);
 }
--- React/nextjs-api/src/app/api/books/route.js
+++ React/nextjs-api/src/app/api/books/route.js
@@ -1,43 +1,64 @@
 import { NextResponse } from "next/server";
+import * as yup from "yup";
 
-const BookData =[
-    {   
-        id:1,
-        title:"Wuthering Heights",
-        author:"Emily Brontë ",
-        year:1847
+const BookData = [
+  {
+    id: 1,
+    title: "Wuthering Heights",
+    author: "Emily Brontë ",
+    year: 1847,
+  },
 
-    },
+  {
+    id: 2,
+    title: "Moby-Dick",
+    author: "Herman Melville",
+    year: 1851,
+  },
 
-     {   
-        id:2,
-        title:"Moby-Dick",
-        author:"Herman Melville",
-        year:1851
+  {
+    id: 3,
+    title: "The Red and the Black",
+    author: "Stendhal",
+    year: 1830,
+  },
+];
 
-    },
-
-     {   
-        id:3,
-        title:"The Red and the Black",
-        author:"Stendhal",
-        year:1830
-
-    },
-
-
-   
-]
+const schema = yup.object().shape({
+  title: yup.string().required("Title is required"),
+  author: yup.string().required("Author is required"),
+  year: yup.string().required("Year is required"),
+});
 
 export async function GET() {
-  return NextResponse.json({ BookData});
+  return NextResponse.json({ BookData });
 }
 
 export async function POST(req) {
-  const body = await req.json();
-  console.log(body);
-  return NextResponse.json({
-    message: "Book is successfully createed",
-    bodyData: body,
-  });
+  try {
+    const body = await req.json();
+    await schema.validate(body, { abortEarly: false });
+    return NextResponse.json({
+      message: "Book is successfully createed",
+      bodyData: body,
+    });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      return NextResponse.json({
+        message: "Validation Failed",
+        errors: error.inner.map((e) => ({
+          path: e.path,
+          message: e.message,
+        })),
+      });
+    }
+
+    return NextResponse.json(
+      {
+        message: "Unexpected Error",
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
 }
